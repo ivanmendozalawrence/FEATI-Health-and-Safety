24e3357ab39a39180237ede8f0e67f6c8c4c5c7e
ivanmendozalawrence <47345247+ivanmendozalawrence@users.noreply.github.com>
2021-09-05 15:49:25 +0800

    Changes

---
+++ resources/JS/Index/news/app.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+
+  let url = "https://newsapi.org/v2/top-headlines?country=ph&category=health&apiKey=fd449250b00d4d0e844148134a0310e5";
+
+  $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "JSON",
+      
+
+      beforeSend: function () {
+          $(".progress").show();
+      },
+
+      complete: function () {
+          $(".progress").hide();
+      },
+
+      success: function (newsdata) {
+          let output = "";
+          let latestNews = newsdata.articles;
+          var imageurl = "";
+          var author = "";
+         var count = 0;
+
+          for (var i in latestNews) {
+              count++;
+
+              if(count > 3){
+                  break;
+              }
+
+              if (latestNews[i].urlToImage == null) {
+               
+                  imageurl = "https://i.ibb.co/m6J6j0H/News.png";
+              } else {
+                  imageurl = latestNews[i].urlToImage;
+              }
+
+              if (latestNews[i].author == null) {
+                  author = latestNews[i].source.name;
+              } else {
+                  author = latestNews[i].author;
+              }
+              console.log(imageurl);
+              output += `
+        <div class="col mb-4">
+        <div class="card">
+          <div class="card-image">
+            <img src="${imageurl}" class="card-img-top" alt="${latestNews[i].title}">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator"><i class="material-icons right">more_vert</i></span>
+            <h6 class="truncate">Title: <a href="${latestNews[i].url}" title="${latestNews[i].title}">${latestNews[i].title}</a></h6>
+            <p><b>Author</b>: ${author} </p>
+            <p><b>News source</b>: ${latestNews[i].source.name} </p>
+            <p><b>Published</b>: ${latestNews[i].publishedAt} </p>
+          </div>
+
+          <div class="card-reveal">
+            <span class="card-title"><i class="material-icons right">close</i></span>
+            <p><b>Description</b>: ${latestNews[i].description}</p>
+          </div>
+
+          <div class="card-action">
+            <a href="${latestNews[i].url}" target="_blank" class="btn">Read More</a>
+          </div>
+         </div>
+        </div>
+      `;
+          }
+
+          if (output !== "") {
+              $("#newsResults").html(output);
+          }
+
+      },
+
+      error: function () {
+          let errorMsg = `<div class="errorMsg center">Some error occured</div>`;
+          $("#newsResults").html(errorMsg);
+      }
+  })
+
+});
